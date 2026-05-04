2bb3dc6afe4aac2190e08a378e07fb707f82372e
Aleksandra <aleksandra.korza@mail.ru>
2026-05-04 14:21:42 +0200

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-]);
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      js,
+      '@stylistic': stylistic,
+    },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/arrow-parens': ['error', 'as-needed', {
+        requireForBlockBody: true,
+      }],
+    },
+  },
+])
